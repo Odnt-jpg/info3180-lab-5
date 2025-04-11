2ebc13abe6fddbc5cc0d36b99c9bbedf37fc9cbf
Odnt <remyalexd2003@gmail.com>
2025-04-11 03:55:11 -0500

    Exercise 2

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from .config import Config
+# import flask migrate here
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import models
 from app import views
--- app/models.py
+++ app/models.py
@@ -1,1 +1,11 @@
 # Add any model classes for Flask-SQLAlchemy here
+from . import db
+class Movie(db.Model):
+    
+    __tablename__ = 'movies'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable = False)
+    description = db.Column(db.Text, nullable = False)
+    poster = db.Column(db.String(255), unique=True)
+    created_at = db.Column(db.DateTime, default=db.func.now(), nullable=False )
